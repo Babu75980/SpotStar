2a6520b90a534ba9e0d08f78a5a0be78097a9710
Babu75980 <132995944+Babu75980@users.noreply.github.com>
2024-08-21 20:20:59 +0530

    Files

---
+++ js/hommovmus.js
@@ -0,0 +1,26 @@
+<script>
+        function hom1(){
+            // var button = document.getElementById("hmm").value
+            // if(button == 11){
+                window.location.href="/html/home.html"
+            // }
+        }   
+        function mus2(){
+            // var button = document.getElementById("hmm").value
+            // if(button == 13){
+            window.location.href="/mini-project1/html/music/musicdirectors.html"
+            // }
+        }
+        function mov3(){
+            // var button = document.getElementById("hmm").value
+            // if(button == 12){
+                window.location.href="/mini-project1/html/movies/moviedirectors.html"
+            // }
+        }
+        <div id="hmm">
+        <button value="11" onclick="hom1()">Home</button >
+        <button value="12" onclick="mus2()">Music</button >
+        <button value="13" onclick="mov3()">Movie</button>
+    </div>
+    </script>
+    
